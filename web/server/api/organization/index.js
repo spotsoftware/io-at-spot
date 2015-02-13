0b0f37e31d12487b57629fbe90b7a274c64699e9
Andrea Rinaldi <andrea.rinaldi@spot.it>
2015-02-13 18:33:14 +0100

    going RESTful with organizations

--- web/server/api/organization/index.js
+++ web/server/api/organization/index.js
@@ -7,8 +7,7 @@ var auth = require('../../auth/auth.service');
 var router = express.Router();
 
 router.get('/', auth.isAuthenticated(), controller.index);
-//router.get('/:organizationId', auth.ensureOrganizationUser(), controller.show);
-router.post('/new', auth.isAuthenticated(), controller.create);
+router.post('/', auth.isAuthenticated(), controller.create);
 router.put('/:organizationId', auth.ensureOrganizationAdmin(), controller.update);
 router.patch('/:organizationId', auth.ensureOrganizationAdmin(), controller.update);
 router.delete('/:organizationId', auth.ensureOrganizationAdmin(), controller.destroy);
