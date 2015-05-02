5b3f9039c31b23853f92397dbcab3d063cf61828
Andrea Rinaldi <andrea.rinaldi@spot.it>
2015-05-02 12:55:07 +0200

    fixed landing page and added notification for pending invites

--- web/client/app/main/main.controller.js
+++ web/client/app/main/main.controller.js
@@ -1,12 +1,32 @@
 'use strict';
 
 angular.module('ioAtSpotApp')
-    .controller('MainCtrl', function ($scope, $http, socket, Auth) {
+    .controller('MainCtrl', function ($scope, $http, socket, Auth, Organizations, messageCenterService) {
 
         $scope.model = {
 
+            // auth
+            authModel: Auth.getAuthModel(),
             isLoggedIn: Auth.isAuthenticated
-
         };
+
+        $scope.actions = {
+
+            search: function () {
+                Organizations.query({}, {}).$promise.then(
+                    function (data) {
+                        if (data.invites && data.invites.length > 0) {
+                            messageCenterService.add('danger', 'You have got some pending invite. Check your organizations info.');
+                        }
+                    },
+                    function (err) {
+                        messageCenterService.add('danger', err.data.error, {
+                            timeout: 3000
+                        });
+                    });
+            }
+        }
+
+        $scope.actions.search();
 
     });
