f2cbc0bdec5f697c96c8ede01896b5b9da53a456
Andrea Corzani <andrea.corzani@spot.it>
2015-05-08 16:40:08 +0200

    -

--- web/client/app/organizations/organizations.service.js
+++ web/client/app/organizations/organizations.service.js
@@ -39,7 +39,7 @@ angular.module('ioAtSpotApp')
                         isArray: false
                     },
                     readUid: {
-                        url: '/api/organizations/:organizationId/readuid',
+                        url: '/api/organizations/:organizationId/uid',
                         params: {
                             organizationId: '@organizationId'
                         },
