4eb526cd046544d019218e16ee4805df9dc6cc4e
Luca Giorgetti <luca.giorgetti@spot.it>
2015-05-25 17:11:22 +0200

    Predisposizione per utilizzo Google+ SDK

---
+++ iOS/IoAtSpot/IoAtSpot/Constants.h
@@ -0,0 +1,15 @@
+//
+//  Constants.h
+//  IoAtSpot
+//
+//  Created by Luca Giorgetti on 25/05/15.
+//  Copyright (c) 2015 SPOT. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Constants : NSObject
+
+extern NSString * const kClientId;
+
+@end
