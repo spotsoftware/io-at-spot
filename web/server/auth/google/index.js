5f271710b15d1518a90be2f0a6e6e9a4dc25ddca
Andrea Corzani <andrea.corzani@spot.it>
2015-10-26 18:03:16 +0100

    auth change

--- web/server/auth/google/index.js
+++ web/server/auth/google/index.js
@@ -33,45 +33,41 @@ router
                 return res.send(err);
             }
 
-            if (req.body.email === body.email) {
+            User.findOne({
+                'email': body.email
+            }, function (err, user) {
 
-                User.findOne({
-                    'email': body.email
-                }, function (err, user) {
+                if (err) {
+                    return res.send(401, err);
+                }
 
-                    if (err) {
-                        return res.send(401, err);
-                    }
+                if (!user) {
+                    return res.send(401, "User not registered.");
+                } else {
+                    //user found, return this user
+                    var token = auth.signToken({
+                        _id: user._id
+                    }, 60 * 24 * 10);
 
-                    if (!user) {
-                        return res.send(401, "User not registered.");
-                    } else {
-                        //user found, return this user
-                        var token = auth.signToken({
-                            _id: user._id
-                        }, 60 * 24 * 10);
-                        
-                        var hash = crypto.createHash('md5').update(token).digest('hex');
-            
-                        user.deviceTokenHash = hash;
+                    var hash = crypto.createHash('md5').update(token).digest('hex');
 
-                        user.save(function(err){
-                            if(err){
-                                return res.json(401, err);
-                            }
+                    user.deviceTokenHash = hash;
 
-                            res.json({
-                                token: token,
-                                tokenHash: hash,
-                                user: user,
-                                type: 'local'
-                            });
+                    user.save(function(err){
+                        if(err){
+                            return res.json(401, err);
+                        }
+
+                        res.json({
+                            token: token,
+                            tokenHash: hash,
+                            user: user,
+                            type: 'local'
                         });
-                    }
-                });
-            } else {
-                return res.send(401, "Invalid token");
-            }
+                    });
+                }
+            });
+
         });
 
     });
