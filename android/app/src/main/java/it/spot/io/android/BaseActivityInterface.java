8b98fc06e58c05479976fc9056a76fe54be7cf60
Andrea Corzani <andrea.corzani@spot.it>
2016-10-05 10:11:34 +0200

    code cleaning

--- android/app/src/main/java/it/spot/io/android/BaseActivityInterface.java
+++
@@ -1,9 +0,0 @@
-package it.spot.io.android;
-
-/**
- * @author a.rinaldi
- */
-public interface BaseActivityInterface {
-
-    // INF: Empty for the moment
-}
--- android/app/src/main/java/it/spot/io/android/http/IDataResponse.java
+++
@@ -1,8 +0,0 @@
-package it.spot.io.android.http;
-
-/**
- * Created by andreacorzani on 03/11/14.
- */
-public interface IDataResponse<T> extends IBaseResponse {
-    public T getData();
-}
